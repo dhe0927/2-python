c2195deb375d6f2d137658c1d5b058c9db387e3e
dhe0927 <103726142+dhe0927@users.noreply.github.com>
2022-05-07 23:42:48 +0900

    Add files via upload

---
+++ 문법/2참고.py
@@ -0,0 +1,16 @@
+user_id = input('id?')
+user_pwd = input('password?')
+
+'''
+if user_pwd == '111111':
+    print('아니요')
+else:
+    print('어디가세요')
+'''
+if user_id == 'egoing':
+    if user_pwd == '111111':
+        print('아니요')
+    else:
+        print('어디가세요')
+else:
+    print('어디가세요')
---
+++ 문법/넘버.py
@@ -0,0 +1,9 @@
+1 "1"
+print(1+1)
+print(True)
+print(False)
+print(1+1)
+print('Hello '+'world')
+print(1==1)
+print(2<1)
+print(1<2)
